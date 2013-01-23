973464e3cd2fd3e9414604a730a2a2867e1832fc
James Gallichio <james@agoodman.com.au>
2013-01-24 10:37:07 +1100

    Fixed a lot of bugs
    - Now wraps everything in 3 special divs to aid in css specificity
    - Responsive CSS is improved
    - Gets the body width, maximises it, calculates the original desired
    layout of the page then stores these values in inline css.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
 	
+	// First, wrap the entire body in 3 divs. This is important for CSS specificity, so that the .row-fluid and .span styles override any user-defined styles
+	
+	$('body div:first-child').wrap('<div id="responsiveWrap1" class="responsive-wrap" />').wrap('<div id="responsiveWrap2" class="responsive-wrap" />').wrap('<div id="responsiveWrap3" class="responsive-wrap" />');
+	
+	
+	// Make all images responsive
 	$('img').each(function(){
 		$(this).css('max-width', '100%');
 	});
 
+	// Get all the elements we want to make responsive. There could be more to add here
+	
 	var theElements = ['div', 
 					'section',
 					'table',
@@ -18,11 +26,18 @@ $(document).ready(function(){
 					'nav',
 					'section',
 					'summary'];
-
-					
+	
+	// Body's width onLoad goes in a variable. We store this because we're about to change it / will need to change back
+	
+	var bodyWidth = $('body').width();
+	
+	// Change body's width to 1280px. This is because we need to see what elements' widths are 'meant' to look like at full width (regardless of what the browser is set to) before we start changing things
+	
+	$('body').css('width','1280px');
+	
 	for (var i=0; i<theElements.length; i++){
 
-		$(theElements[i]).each(function(){
+		$(theElements[i]).not('.responsive-wrap').each(function(){
 			var theWidth = $(this).width();
 			var parentWidth = $(this).parent().width();
 			$(this).css('max-width', theWidth + "px"); 
@@ -31,63 +46,30 @@ $(document).ready(function(){
 				$(this).addClass("row-fluid").css("width", "100%");
 				$(this).children('div:has(div:eq(1))').addClass("rowChild");
 			}
-			// Might not need this level of detail
-			$('.rowChild').children('div:has(div:eq(1))').addClass("JamesThing");
+			
 		});
-
-
 		
-
-		
-		
-		$(theElements[i]).not('.row-fluid').each(function(i, elemen){
+		$(theElements[i]).not('.row-fluid').not('.responsive-wrap').each(function(i, elemen){
 			var theWidth = $(this).width();
 			var parentWidth = $(this).parent().width();			
 			$(this).css('max-width', theWidth + "px"); 
 			
 			var percWidth = $(this).width() / $(this).parent().width() * 100 - 1;
-		/*	if (percWidth < 6.382978723404255){
-				$(this).addClass("span1");
-			}else if((percWidth > 6.382978723404255) && (percWidth < 14.893617021276595)){
-				$(this).addClass("span2");
-			}else if((percWidth >14.893617021276595) && (percWidth < 23.404255319148934)){
-				$(this).addClass("span3");
-			}else if((percWidth >23.404255319148934) && (percWidth < 31.914893617021278)){
-				$(this).addClass("span4");
-			}else if((percWidth >31.914893617021278) && (percWidth < 40.42553191489362)){
-				$(this).addClass("span5");
-			}else if((percWidth >40.42553191489362) && (percWidth < 48.93617021276595)){
-				$(this).addClass("span6");
-			}else if((percWidth >48.93617021276595) && (percWidth < 57.44680851063829)){
-				$(this).addClass("span7");
-			}else if((percWidth >57.44680851063829) && (percWidth < 65.95744680851064)){
-				$(this).addClass("span8");
-			}else if((percWidth >65.95744680851064) && (percWidth < 74.46808510638297)){
-				$(this).addClass("span9");
-			}else if((percWidth >74.46808510638297) && (percWidth < 82.97872340425532)){
-				$(this).addClass("span10");
-			}else if((percWidth >82.97872340425532) && (percWidth < 91.48936170212765)){
-				$(this).addClass("span11");
-			}else if((percWidth >91.48936170212765) && (percWidth <= 101)){
-				$(this).addClass("span12");
-			}else{
-				console.log("What's this?");
+				
+			function spanClass(percentWidth) {
+				return 'span' + Math.round(percentWidth / (100/12));
 			}
-		*/
-		
-		function spanClass(percentWidth) {
-			return 'span' + Math.round(percentWidth / (100/12));
-		}
-		
-		$(this).addClass(spanClass(percWidth));
-
-
-		
-		
 			
+			$(this).addClass(spanClass(percWidth));			
 
 		});
 	}
+	
+	// Change the body size back to the original width, then clear the width css to avoid conflicts
+	
+	$('body').css('width', bodyWidth);
+	$('body').css('width', "");
+	
 	
 	//Get all .span12 elements which have child divs and convert to .row-fluid
 	
@@ -98,30 +80,6 @@ $(document).ready(function(){
 	// Get remaining .span12 elements (which have no child divs) and wrap in a .row-fluid
 	
 	$('.span12').wrap("<div class='row-fluid' />");
-
-	
-
-$('.row-fluid').each(function (i, ele) {
-    var total = 0;
-    var currentSpans = [];
-
-    $(ele).children().each(function (i2, spans) {
-        if (spans.className.indexOf('span') != -1) {
-
-			
-            total += parseInt(spans.className.replace(/[A-Za-z$-]/g, ""), 10);
-            currentSpans.push($(spans).attr('class'));
-        }
-
-    });
-
-
-    console.log(currentSpans, total);
-	if (total < 12){
-		
-		
-	}
-});
 
 	
 });
